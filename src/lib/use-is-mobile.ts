14579543f3ddfb53d70a134649eef2d0160ed57f
Yinka Adedire <yinka.adedire@internetbrands.com>
2025-07-29 19:23:43 +0100

    add mobile detection hook

---
+++ src/lib/use-is-mobile.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function useIsMobile(mobileBreakpoint = 768) {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${mobileBreakpoint - 1}px)`);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < mobileBreakpoint);
+    };
+    mql.addEventListener('change', onChange);
+    setIsMobile(window.innerWidth < mobileBreakpoint);
+    return () => mql.removeEventListener('change', onChange);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return !!isMobile;
+}
