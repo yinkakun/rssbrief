4e2654eb6401712cc43ee25128629688b67fb97c
Yinka Adedire <yinka.adedire@internetbrands.com>
2025-08-03 17:03:35 +0100

    remove description from toast component

--- src/ui/toaster.tsx
+++ src/ui/toaster.tsx
@@ -8,16 +8,14 @@ export const Toaster = ({ ...props }: ToasterProps) => {
 
 interface ToastProps {
   title: string;
-  description?: string;
   id: string | number;
 }
 
-function Toast({ title, id, description }: ToastProps) {
+function Toast({ title, id }: ToastProps) {
   return (
     <div className="xw-full flex w-md items-center rounded-2xl bg-white p-4 ring-1 ring-black/5">
       <div className="w-full">
         <p className="text-sm font-medium text-gray-900">{title}</p>
-        {description && <p className="mt-1 text-xs text-gray-500">{description}</p>}
       </div>
       <div className="ml-5 shrink-0">
         <Button variant="default" className="text-xs" size="sm" onClick={() => sonnerToast.dismiss(id)}>
